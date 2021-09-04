979f78a2d4575eb4498fd59188039d7c6cb07f20
StefanChik <73401261+stefanchik777@users.noreply.github.com>
2021-09-04 10:57:09 +0300

    Add files via upload

---
+++ js/bootstrap.min.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>CDN 404</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+	<meta property="og:url" content="https://cdn.0880.xyz">
+	<meta property="og:title" content="404">
+	<meta property="twitter:image" content="https://cdn.0880.xyz/8h10glyr18/8h10glyr18.jpg">
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>404</h1>
+			</div>
+			<a href="https://cdn.0880.xyz">Home</a>
+		</div>
+	</div>
+
+</body>
+
+</html>
